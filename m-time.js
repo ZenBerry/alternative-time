f52274dad32405036d05c54d1689eb7f86d08979
Eugene ZenBerry <eugeneberezhnoy@gmail.com>
2023-09-09 21:18:39 +0300

    shit what

--- m-time.js
+++ m-time.js
@@ -1,22 +1,21 @@
 const now = new Date();
-let s = 0;
-let h = 0;
-let ms = 0;
+let h = now.getHours();
+let m = now.getMinutes();
+let s = now.getSeconds()+2;
 let newMs = 960;
-console.log(date);
 
 setInterval(() => {
-  console.log(`${h}: ${s}: ${ms}`);
-  ms+= 1;
-  if (ms > 60) {
-    ms = 0;
-    s += 1;
-    if (s > 60) {
-      s = 0;
-      h +=1;
+  console.log(`${h}: ${m}: ${s}`);
+  s += 1;
+  if (s > 59) {
+    s = 0;
+    m += 1;
+    if (m > 59) {
+      m = 0;
+      h += 1;
       if (h > 25) {
         h = 0;
       }
     }
   }
-}, newMs);
+}, newMs)
